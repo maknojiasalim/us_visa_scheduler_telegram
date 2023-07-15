76fa62cd9be3ee41485083e6b4b5fd118c606a2a
shcheglovnd <shcheglovnd@yandex-team.ru>
2023-07-15 09:53:35 +0000

    Many users for no payment script now with yaml config

--- visa_no_payment.py
+++ visa_no_payment.py
@@ -5,6 +5,7 @@ import random
 import requests
 import configparser
 import traceback
+import yaml
 from datetime import datetime
 
 from selenium import webdriver
@@ -15,73 +16,48 @@ from selenium.webdriver.support.ui import WebDriverWait as Wait
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-from embassy import *
-
 parser = argparse.ArgumentParser()
 parser.add_argument('--config', default='config.ini')
 args = parser.parse_args()
 
-config = configparser.ConfigParser()
-config.read(args.config)
-
-# Personal Info:
-# Account and current appointment info from https://ais.usvisa-info.com
-USERNAME = config['PERSONAL_INFO']['USERNAME']
-PASSWORD = config['PERSONAL_INFO']['PASSWORD']
-# Find SCHEDULE_ID in re-schedule page link:
-# https://ais.usvisa-info.com/en-am/niv/schedule/{SCHEDULE_ID}/appointment
-SCHEDULE_ID = config['PERSONAL_INFO']['SCHEDULE_ID']
-GROUP_ID = config['PERSONAL_INFO']['GROUP_ID']
-# Embassy Section:
-YOUR_EMBASSY = config['PERSONAL_INFO']['YOUR_EMBASSY']
-EMBASSY = Embassies[YOUR_EMBASSY][0]
-FACILITY_ID = Embassies[YOUR_EMBASSY][1]
-REGEX_CONTINUE = Embassies[YOUR_EMBASSY][2]
-
-# Notification:
-TELEGRAM_BOT_TOKEN = config['NOTIFICATION']['TELEGRAM_BOT_TOKEN']
-TELEGRAM_CHAT_ID = config['NOTIFICATION']['TELEGRAM_CHAT_ID']
-# TELEGRAM_MESSAGE_THREAD_ID = config['NOTIFICATION']['TELEGRAM_MESSAGE_THREAD_ID']
+config = {}
+with open(args.config) as f:
+    config = yaml.load(f, Loader=yaml.loader.SafeLoader)
 
 # Time Section:
 minute = 60
 hour = 60 * minute
 # Time between steps (interactions with forms)
-STEP_TIME = 0.5
-# Time between retries/checks for available dates (seconds)
-RETRY_TIME_L_BOUND = config['TIME'].getfloat('RETRY_TIME_L_BOUND')
-RETRY_TIME_U_BOUND = config['TIME'].getfloat('RETRY_TIME_U_BOUND')
-# Cooling down after WORK_LIMIT_TIME hours of work (Avoiding Ban)
-WORK_LIMIT_TIME = config['TIME'].getfloat('WORK_LIMIT_TIME')
-WORK_COOLDOWN_TIME = config['TIME'].getfloat('WORK_COOLDOWN_TIME')
-# Temporary Banned (empty list): wait COOLDOWN_TIME hours
-BAN_COOLDOWN_TIME = config['TIME'].getfloat('BAN_COOLDOWN_TIME')
+STEP_TIME = 3.0
 
-# CHROMEDRIVER
-# Details for the script to control Chrome
-LOCAL_USE = config['CHROMEDRIVER'].getboolean('LOCAL_USE')
-# Optional: HUB_ADDRESS is mandatory only when LOCAL_USE = False
-HUB_ADDRESS = config['CHROMEDRIVER']['HUB_ADDRESS']
 
-SIGN_IN_LINK = f"https://ais.usvisa-info.com/{EMBASSY}/niv/users/sign_in"
-APPOINTMENT_URL = f"https://ais.usvisa-info.com/{EMBASSY}/niv/schedule/{SCHEDULE_ID}/appointment"
-PAYMENT_URL = f"https://ais.usvisa-info.com/{EMBASSY}/niv/schedule/{SCHEDULE_ID}/payment"
-DATE_URL = f"https://ais.usvisa-info.com/{EMBASSY}/niv/schedule/{SCHEDULE_ID}/appointment/days/{FACILITY_ID}.json?appointments[expedite]=false"
-TIME_URL = f"https://ais.usvisa-info.com/{EMBASSY}/niv/schedule/{SCHEDULE_ID}/appointment/times/{FACILITY_ID}.json?date=%s&appointments[expedite]=false"
-SIGN_OUT_LINK = f"https://ais.usvisa-info.com/{EMBASSY}/niv/users/sign_out"
-GROUP_LINK = f"https://ais.usvisa-info.com/en-il/niv/groups/{GROUP_ID}"
+def get_links_for_embassy(user_config, embassy_config):
+    schedule_id = user_config['schedule_id']
+    group_id = user_config['group_id']
+    country_code = embassy_config['country_code']
+    facility_id = embassy_config['facility_id']
+    return {
+        'sign_in_link': f"https://ais.usvisa-info.com/{country_code}/niv/users/sign_in",
+        'appointment_url': f"https://ais.usvisa-info.com/{country_code}/niv/schedule/{schedule_id}/appointment",
+        'payment_url': f"https://ais.usvisa-info.com/{country_code}/niv/schedule/{schedule_id}/payment",
+        'date_url': f"https://ais.usvisa-info.com/{country_code}/niv/schedule/{schedule_id}/appointment/days/{facility_id}.json?appointments[expedite]=false",
+        'time_url': f"https://ais.usvisa-info.com/{country_code}/niv/schedule/{schedule_id}/appointment/times/{facility_id}.json?date=%s&appointments[expedite]=false",
+        'sign_out_link': f"https://ais.usvisa-info.com/{country_code}/niv/users/sign_out",
+        'group_link': f"https://ais.usvisa-info.com/en-il/niv/groups/{group_id}",
+    }
 
 
 def send_notification(title, msg):
-    print(f"Sending notification!")
-    if TELEGRAM_BOT_TOKEN:
-        url = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage'
-        data = {
-            'chat_id': TELEGRAM_CHAT_ID,
-            # 'message_thread_id': TELEGRAM_MESSAGE_THREAD_ID,
-            'text': msg,
-        }
-        requests.post(url, data)
+    data = {
+        'chat_id': config['telegram']['chat_id'],
+        'text': msg,
+    }
+    token = config['telegram']['bot_token']
+    url = f'https://api.telegram.org/bot{token}/sendMessage'
+    if config['telegram']['thread_id']:
+        data.update({'message_thread_id': config['telegram']['thread_id']})
+    print(f"Sending notification {data}")
+    requests.post(url, data)
 
 
 def auto_action(label, find_by, el_type, action, value, sleep_time=0):
@@ -111,23 +87,23 @@ def auto_action(label, find_by, el_type, action, value, sleep_time=0):
         time.sleep(sleep_time)
 
 
-def start_process():
+def start_process(user_config, embassy_config, embassy_links):
     # Bypass reCAPTCHA
-    driver.get(SIGN_IN_LINK)
+    driver.get(embassy_links['sign_in_link'])
     time.sleep(STEP_TIME)
     Wait(driver, 60).until(EC.presence_of_element_located((By.NAME, "commit")))
     auto_action("Click bounce", "xpath", '//a[@class="down-arrow bounce"]', "click", "", STEP_TIME)
-    auto_action("Email", "id", "user_email", "send", USERNAME, STEP_TIME)
-    auto_action("Password", "id", "user_password", "send", PASSWORD, STEP_TIME)
+    auto_action("Email", "id", "user_email", "send", user_config['email'], STEP_TIME)
+    auto_action("Password", "id", "user_password", "send", user_config['password'], STEP_TIME)
     auto_action("Privacy", "class", "icheckbox", "click", "", STEP_TIME)
     auto_action("Enter Panel", "name", "commit", "click", "", STEP_TIME)
-    Wait(driver, 60).until(EC.presence_of_element_located((By.XPATH, "//a[contains(text(), '" + REGEX_CONTINUE + "')]")))
+    Wait(driver, 60).until(EC.presence_of_element_located((By.XPATH, "//a[contains(text(), '" + embassy_config['continue'] + "')]")))
     print("\n\tlogin successful!\n")
     print(f'Cookies: {driver.get_cookies()}')
 
 
-def get_first_available_appointments():
-    driver.get(PAYMENT_URL)
+def get_first_available_appointments(embassy_links):
+    driver.get(embassy_links['payment_url'])
     res = {}
     for i in range(1, 3):
         location = driver.find_elements(by=By.XPATH, value=f'//*[@id="paymentOptions"]/div[2]/table/tbody/tr[{i}]/td[1]')
@@ -140,7 +116,7 @@ def get_first_available_appointments():
     return res
 
 
-if LOCAL_USE:
+if config['chrome_driver']['local_use']:
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--disable-extensions")
@@ -148,41 +124,63 @@ if LOCAL_USE:
     options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 else:
-    driver = webdriver.Remote(command_executor=HUB_ADDRESS, options=webdriver.ChromeOptions())
+    driver = webdriver.Remote(command_executor=config['chrome_driver']['hub_address'], options=webdriver.ChromeOptions())
+
+def get_user_id():
+    id = 0
+    while True:
+        yield id
+        id = (id + 1) % len(config['users'])
 
 
 if __name__ == "__main__":
     first_loop = True
     prev_available_appointments = None
+    get_user = get_user_id()
+    user_id = None
+    banned_count = 0
     while 1:
         try:
             if first_loop:
+                user_id = next(get_user)
+                embassy_config = config['embassies'][0]
+                links = get_links_for_embassy(config['users'][user_id], embassy_config)
                 t0 = time.time()
                 total_time = 0
                 Req_count = 0
-                start_process()
+                start_process(
+                    user_config=config['users'][user_id],
+                    embassy_config=embassy_config,
+                    embassy_links=links,
+                )
                 first_loop = False
 
             Req_count += 1
-            msg = "-" * 60 + f"\nRequest count: {Req_count}, Log time: {datetime.today()}\n"
-            print(msg)
-            appointments = get_first_available_appointments()
+            print("-" * 60 + f"\nRequest count: {Req_count}, Log time: {datetime.today()}\n")
+            appointments = get_first_available_appointments(links)
             if all(x == "No Appointments Available" for x in appointments.values()):
-                print(f'Probably banned, cooldown for {BAN_COOLDOWN_TIME}h')
-                time.sleep(BAN_COOLDOWN_TIME * hour)
+                print(f"Probably user {config['users'][user_id]['email']} is banned")
+                banned_count += 1
+                if banned_count == len(config['users']):
+                    print(f"All users are banned, resting for {config['time']['ban_cooldown_hours']}h")
+                    time.sleep(config['time']['ban_cooldown_hours'] * hour)
+                    banned_count = 0
+                driver.get(links['sign_out_link'])
+                first_loop = True
+                continue
             if appointments is not None and appointments != prev_available_appointments:
                 send_notification('SUCCESS', json.dumps(appointments, sort_keys=True))
             else:
-                RETRY_WAIT_TIME = random.randint(RETRY_TIME_L_BOUND, RETRY_TIME_U_BOUND)
+                RETRY_WAIT_TIME = random.randint(config['time']['retry_lower_bound'], config['time']['retry_upper_bound'])
                 t1 = time.time()
                 total_time = t1 - t0
                 msg = "\nWorking Time:  ~ {:.2f} minutes".format(total_time/minute)
                 print(msg)
-                if total_time > WORK_LIMIT_TIME * hour:
+                if total_time > config['time']['work_limit_hours'] * hour:
                     # Let program rest a little
-                    print("REST", f"Break-time after {WORK_LIMIT_TIME} hours | Repeated {Req_count} times")
-                    driver.get(SIGN_OUT_LINK)
-                    time.sleep(WORK_COOLDOWN_TIME * hour)
+                    print("REST", f"Break-time after {config['time']['work_limit_hours']} hours | Repeated {Req_count} times")
+                    driver.get(links['sign_out_link'])
+                    time.sleep(config['time']['work_cooldown_hours'] * hour)
                     first_loop = True
                 else:
                     msg = "Retry Wait Time: "+ str(RETRY_WAIT_TIME)+ " seconds"
@@ -192,14 +190,8 @@ if __name__ == "__main__":
                 prev_available_appointments = appointments
         except:
             # Exception Occured
-            msg = f"Break the loop after exception! I will continue in a few minutes\n"
+            print(f"Break the loop after exception! I will continue in a few minutes\n")
             END_MSG_TITLE = "EXCEPTION"
             traceback.print_exc()
             # send_notification(END_MSG_TITLE, msg)
-            time.sleep(random.randint(RETRY_TIME_L_BOUND, RETRY_TIME_U_BOUND))
-
-print(msg)
-send_notification(END_MSG_TITLE, msg)
-driver.get(SIGN_OUT_LINK)
-driver.stop_client()
-driver.quit()
+            time.sleep(random.randint(config['time']['retry_lower_bound'], config['time']['retry_upper_bound']))
